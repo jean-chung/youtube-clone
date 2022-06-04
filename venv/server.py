b6dd0343342834187229d764a46ca2c53e1160e5
Jean Chung <jean-chung@192-168-1-106.tpgi.com.au>
2022-06-04 17:02:13 +1000

    added watch template

--- venv/server.py
+++ venv/server.py
@@ -1,7 +1,13 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route('/watch/')
+@app.route('/watch/<name>')
+def watch(name=None):
+    return render_template('watch.html', name=name)
