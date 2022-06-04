de7b0c3897e7278fe8a19a6fbc91d48bdc0f6ab7
Jean Chung <jean-chung@192-168-1-106.tpgi.com.au>
2022-06-04 22:10:17 +1000

    added more thumbnails and videos

--- venv/server.py
+++ venv/server.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from flask import render_template
+import json
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template('home.html')
+    with open('db.json', 'r') as f:
+        db = json.load(f)
+
+    return render_template('home.html', db=db)
 
 @app.route('/watch/')
 @app.route('/watch/<name>')
